c874ca05b6de8bb8e9c99166b461e6650e3644b3
jason <jtkarita@ucdavis.edu>
2014-07-23 17:48:42 -0700

    added mockup, added new features
    new feature: remembers last used directory when opening files
    new feature: editable CODE, TIME columns in event list window
    new feature: BDF feedback window

--- NOTE_binlisterLibrary.m
+++ NOTE_binlisterLibrary.m
@@ -23,7 +23,7 @@ ELISTfilename   = fullfile(pwd,'testData','ELIST-test.txt');
     ,  []               ...         % ignoreCodeArray
     ,  0                );          % reportable
 
-
+%% Display EVENTLIST to GUI
 hUITableELIST               = handle(handles.uitableELIST);
 hUITableELIST.data          = squeeze(struct2cell(EVENTLIST.eventinfo))';
 hUITableELIST.columnWidth   = 'auto';
@@ -35,6 +35,20 @@ hUITableELIST.columnName    = fieldnames(EVENTLIST.eventinfo);
 %
 %
 
+% Load in an ELIST-file & parse it into an ELIST-structure & display it to
+% the GUI
+[fileName, pathName] = uigetfile('*.txt', 'Select an event list file');
+ELISTfilename       = fullfile(pathName,fileName);
+
+% Load file into ELIST-structure via READEVENTLIST
+% ELISTfilename   = fullfile(pwd,'testData','ELIST-test.txt');
+[~, eventList]  = readeventlist([], ELISTfilename);
+
+% Display ELIST-struct to GUI
+hUITableELIST               = handle(handles.uitableELIST);
+hUITableELIST.data          = squeeze(struct2cell(eventList.eventinfo))';
+hUITableELIST.columnWidth   = 'auto';
+hUITableELIST.columnName    = fieldnames(eventList.eventinfo);
 
 
 %% Step 3 - ADD BINLISTER FUNCTIONALITY
@@ -63,7 +77,7 @@ tmpEEG = pop_binlister( [] ...
 
 
 %% Example: read text file lines as cell array of strings
-fid = fopen( fullfile(pwd,'testData','ELIST-test.txt') );
+fid = fopen( fullfile(pwd,'testData','BDF-test.txt') );
 str = textscan(fid, '%s', 'Delimiter','\n'); str = str{1};
 fclose(fid);
 
@@ -72,14 +86,14 @@ hFig = figure('Menubar','none', 'Toolbar','none');
 hPan = uipanel(hFig, 'Title','Display window', ...
     'Units','normalized', 'Position',[0.05 0.05 0.9 0.9]);
 hEdit = uicontrol(hPan ...
-    , 'Style','edit' ...
-    , 'FontSize',9 ...
-    , 'Min',0 ...
-    , 'Max',2 ...
-    , 'HorizontalAlignment','left' ...
-    , 'Units','normalized' ...
-    , 'Position',[0 0 1 1] ...
-    , 'String',str);
+    , 'Style'               ,'edit' ...
+    , 'FontSize'            ,9 ...
+    , 'Min'                 ,0 ...
+    , 'Max'                 ,2 ...
+    , 'HorizontalAlignment' ,'left' ...
+    , 'Units'               ,'normalized' ...
+    , 'Position'            ,[0 0 1 1] ...
+    , 'String'              ,str);
 
 % # enable horizontal scrolling
 jEdit       = findjobj(hEdit);
